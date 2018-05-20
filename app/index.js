9388c28c00cb21a71bbb03bb5173e00c0b575519
Loïc Hamet <loic.hamet@gmail.com>
2018-05-20 17:31:19 +0200

    Init mongo connection

--- app/index.js
+++ app/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const log4js = require('log4js');
 const config = require('../config');
 
 const app = express();
 const logger = log4js.getLogger('app');
+
+mongoose.connect(config.MONGO_URI);
+mongoose.connection.on('error', () => logger.error('Error on db connection'));
+mongoose.connection.once('open', () => logger.debug(`DB connected on ${config.MONGO_URI}`));
 
 app.get('/', (req, res) => {
   res.send('Loot app');
