446c3577c4bcf30c1cdba37f50c1c09d7b85ae1f
Loïc Hamet <loic.hamet@gmail.com>
2018-05-27 01:07:52 +0200

    load scss

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const context = path.resolve(__dirname, '..');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   context,
@@ -12,7 +15,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js*/,
+        test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
@@ -21,7 +24,33 @@ module.exports = {
             plugins: ['transform-object-rest-spread']
           }
         }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          },
+        ]
       }
     ]
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.scss'],
+    alias: {
+      scss: `${context}/src/scss`
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'app.css'
+    })
+  ],
 };
