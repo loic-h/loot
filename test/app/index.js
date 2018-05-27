3324f06765ec187e0996979b4bb5fb478e861ff3
Loïc Hamet <loic.hamet@gmail.com>
2018-05-27 12:06:24 +0200

    remove redirect test

--- test/app/index.js
+++ test/app/index.js
@@ -1,23 +1,3 @@
 require('./models/post');
 require('./controllers/post');
 require('./routes/api/post');
-
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../../app');
-
-chai.use(chaiHttp);
-
-const expect = chai.expect;
-
-describe('/GET root', () => {
-  it('should redirect to /posts', done => {
-    chai.request(app)
-      .get('/')
-      .redirects(0)
-      .end((err, res) => {
-        expect(res).to.redirectTo('/posts');
-        done();
-      });
-  });
-});
